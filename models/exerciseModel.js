c95793f82082baf16e8e38957a4f5092b7af6e28
Skyler R <skyler.rencher@gmail.com>
2020-12-29 23:27:29 -0500

    added a schema to the exerciseModel.js.

--- models/exerciseModel.js
+++ models/exerciseModel.js
@@ -3,19 +3,28 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const exerciseSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter a name for transaction"
-  },
-  value: {
-    type: Number,
-    required: "Enter an amount"
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
+    type: {
+        type: String,
+        trim: true,
+        required: "Enter the type of workout"
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: "Enter a name for the exercise"
+    },
+    weight: {
+        type: Number,
+        required: "Enter the weight used in the workout"
+    },
+    reps: {
+        type: Number,
+        required: "Enter the amount of reps completed"
+    },
+    duration: {
+        type: Number,
+        required: "Enter the length of the time of the exercise"
+    }
 });
 
 const Exercise = mongoose.model("Exercise", exerciseSchema);
