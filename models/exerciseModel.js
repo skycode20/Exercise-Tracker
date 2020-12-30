e6a30f18c4d6c7bb8eee58a76dc0ebf39f9c6b2c
Skyler R <skyler.rencher@gmail.com>
2020-12-30 14:02:11 -0500

    added the sets and distance objects to the workoutSchema.

--- models/exerciseModel.js
+++ models/exerciseModel.js
@@ -3,28 +3,46 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    type: {
-        type: String,
-        trim: true,
-        required: "Enter the type of workout"
-    },
-    name: {
-        type: String,
-        trim: true,
-        required: "Enter a name for the exercise"
-    },
-    weight: {
-        type: Number,
-        required: "Enter the weight used in the workout"
-    },
-    reps: {
-        type: Number,
-        required: "Enter the amount of reps completed"
-    },
-    duration: {
-        type: Number,
-        required: "Enter the length of the time of the exercise"
-    }
+    
+        day: {
+            type: Date,
+            default: Date.now,
+            required: true
+        },
+        exercises: [
+            {
+            type: {
+                type: String,
+                trim: true,
+                required: "Enter the type of workout"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter a name for the exercise"
+            },
+            duration: {
+                type: Number,
+                required: "Enter the length of the time of the exercise"
+            },
+            weight: {
+                type: Number,
+                required: "Enter the weight used in the workout"
+            },
+            sets: {
+                type: Number,
+                required: "Enter the amount of sets performed"
+            },
+            reps: {
+                type: Number,
+                required: "Enter the amount of reps completed"
+            },
+            distance: {
+                type: Number,
+                required: "Enter the distance traveled during the exercise"
+            }
+        }
+    ]
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
