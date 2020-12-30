1ad8a6367b8965553ffd4b2f95668d4082e09e38
Skyler R <skyler.rencher@gmail.com>
2020-12-30 13:07:35 -0500

    commented out the post route in server.js. created an index.js file inside of models directory.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const Exercise = require("./models/exerciseModel.js");
+// const Workout = require("./models/exerciseModel.js");
 const app = express();
 
 app.use(logger("dev"));
@@ -16,22 +16,24 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
 });
 
 // routes
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
 
-app.post("/submit", ({ body }, res) => {
-    Exercise.create(body)
-        .then(dbUser => {
-            res.json(dbUser);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
+// app.post("/submit", ({ body }, res) => {
+//     Workout.create(body)
+//         .then(dbUser => {
+//             res.json(dbUser);
+//         })
+//         .catch(err => {
+//             res.json(err);
+//         });
+// });
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
