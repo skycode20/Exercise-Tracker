36631d36b1482bc14312606e839550743f847788
Skyler R <skyler.rencher@gmail.com>
2020-12-31 00:42:47 -0500

    changed the index route in view.js a little bit using the path.join method.

--- routes/view.js
+++ routes/view.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const path = require("path");
 
 router.get("/", (req, res) => {
-    res.send(index.html);
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 })
 
 router.get("/exercise", (req, res) => {
